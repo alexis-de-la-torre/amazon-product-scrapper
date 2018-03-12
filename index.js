cab7fcad45a2364c0a9a7fa6f2774ba8394f3c8f
Alexis De La Torre <contacto@alexisdelatorre.com>
2018-03-11 22:22:47 -0700

    all ready for tweaks

--- index.js
+++ index.js
@@ -3,25 +3,35 @@ const R = require('ramda')
 const { render } = require('prettyjson')
 const { encaseP } = require('fluture')
 const { get } =  require('axios')
+const sample = require('lodash.samplesize')
 
-const categories = require('./categories')
 const product = require('./product')
+const categories = require('./categories')
+const category = require('./category')
 
-categories
-.fork(console.log, console.log)
+const dev = process.env.NODE_ENV !== 'production'
 
-// const ids = [
-//    'B078W1HYH8',
-//    'B000QSNYGI',
-//    'B079K48DNT',
-//    'B008GVVUFE',
-// ]
-//
-// const init = () =>
-//   Future.of(ids)
-//   .map(R.map(product))
-//   .chain(parallel(1))
-//   .map(render)
-//   .fork(console.log, console.log)
-//
-// init()
+const data =
+  categories
+  .map(x => dev ? sample(x) : x)
+  .map(R.map(({ name, link }) => category(link).map(products => ({ name, products }))))
+  .map(R.map(R.map(({ name, products }) => products.map(asin => ({ name, asin })))))
+  .chain(parallel(1))
+  .map(R.flatten)
+
+const program = name => R.pipe(
+  Future.of,
+  R.map(R.tap(asin => console.log(`${name} :: ${asin}`))),
+  R.chain(product),
+)
+
+// program('Home & Kitchen')('B014WOWNAY').fork(console.log, console.log)
+
+const init = () =>
+  data
+  .map(x => dev ? sample(x, 2) : x)
+  .map(R.map(({ name, asin }) => program(name)(asin)))
+  .chain(parallel(1))
+  .fork(console.log, console.log)
+
+init()
