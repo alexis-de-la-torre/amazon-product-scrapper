1a095e9170e3019ae40603a0fde2bf96dce11340
Alexis De La Torre <contacto@alexisdelatorre.com>
2018-03-13 23:48:11 -0700

    use file for test categories

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const { parallel, Future } = require('fluture')
 const R = require('ramda')
 const { render } = require('prettyjson')
-const { encaseP } = require('fluture')
+const { encaseP, encaseN2 } = require('fluture')
 const { get } =  require('axios')
 const sample = require('lodash.samplesize')
+const { readFile } = require('fs')
 
 const product = require('./product')
-const categories = require('./categories')
 const category = require('./category')
 
 const dev = process.env.NODE_ENV !== 'production'
 
 const data =
-  categories
-  .map(R.reject(R.propEq('name', 'Magazine Subscriptions')))
+  encaseN2(readFile, 'tops.txt', 'utf-8')
+  .map(R.split('\n\n'))
+  .map(R.map(R.split('\n')))
+  .map(R.map(R.zipObj([ 'name', 'link' ])))
   .map(x => dev ? sample(x) : x)
   .map(R.map(({ name, link }) => category(link).map(products => ({ name, products }))))
   .map(R.map(R.map(({ name, products }) => products.map(asin => ({ name, asin })))))
