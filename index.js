f75435792cfd1f556cc784f38b6a2c8ae5c97bef
Alexis De La Torre <contacto@alexisdelatorre.com>
2018-03-19 21:38:59 +0000

    bug fix: forgot to add offers

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const getInformation = require('./information')
 const getOffers = require('./offers')
 
 const getProduct = asin =>
-  getInformation(asin)
-  // parallel(1, [ getInformation(asin), getOffers(asin) ])
-  // .map(([ product, offers ]) => ({ ...product, offers }))
+  parallel(1, [ getInformation(asin), getOffers(asin) ])
+  .map(([ product, offers ]) => ({ ...product, offers }))
 
 module.exports = getProduct
